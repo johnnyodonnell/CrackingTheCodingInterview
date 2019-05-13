aa7d9061e71d3cb5c113ebb9daffe07bd63be94e
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-05-13 11:26:09 -0400

    Add solutions to 1.1

---
+++ IX-Interview-Questions/Ch-1/1.1/cpp.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+
+
+using namespace std;
+
+/*
+ * Runs O(n * log2(n))
+ *
+ */
+bool is_unique_no_extra_space(const string str) {
+    string str_copy {str};
+    sort(str_copy.begin(), str_copy.end());
+    for (int i = 1; i < str_copy.length(); i++) {
+        if (str_copy.at(i) == str_copy.at(i - 1)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/*
+ * Runs O(n)
+ *
+ */
+bool is_unique(const string str) {
+    unordered_map<char, bool> char_exists {};
+    for (auto ch : str) {
+        if (char_exists.find(ch) != char_exists.end()) {
+            return false;
+        } else {
+            char_exists[ch] = true;
+        }
+    }
+    return true;
+}
+
+void run_is_unique(const string str) {
+    cout << "Is '" << str << "' unique? " << is_unique_no_extra_space(str) << endl;
+}
+
+int main() {
+    run_is_unique("");
+    run_is_unique("Johnny is cool");
+    run_is_unique("unique");
+    run_is_unique("abcdefg");
+    run_is_unique("Jay Z");
+}
+
