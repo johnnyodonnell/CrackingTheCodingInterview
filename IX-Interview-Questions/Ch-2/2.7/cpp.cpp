1c2c8e003e449ab4feb876d0618ec8a89449fa67
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-05-13 21:38:31 -0400

    Add solution for 2.7

---
+++ IX-Interview-Questions/Ch-2/2.7/cpp.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <forward_list>
+
+
+using namespace std;
+
+/*
+ * Runs O(n)
+ *
+ */
+bool intersect(
+        forward_list<int>::const_iterator node1,
+        const forward_list<int>::const_iterator end1,
+        forward_list<int>::const_iterator node2,
+        const forward_list<int>::const_iterator end2) {
+    auto node1_copy = node1;
+    while (node1_copy != end1) {
+        if (node1_copy == node2) {
+            return true;
+        }
+        node1_copy++;
+    }
+
+    auto node2_copy = node2;
+    while (node2_copy != end2) {
+        if (node1 == node2_copy) {
+            return true;
+        }
+        node2_copy++;
+    }
+
+    return false;
+}
+
+int main() {
+    forward_list<int> list1 {1, 2, 3, 4};
+    forward_list<int> list2 {1, 2, 3, 4};
+
+    cout << "Test 1: "
+        << intersect(list1.begin(), list1.end(), list1.begin(), list1.end())
+        << endl;
+    cout << "Test 2: "
+        << intersect(list1.begin(), list1.end(), list2.begin(), list2.end())
+        << endl;
+    cout << "Test 3: "
+        << intersect(
+                list1.begin(), list1.end(),
+                next(list1.begin(), 2), list1.end())
+        << endl;
+    cout << "Test 3: "
+        << intersect(
+                next(list1.begin(), 1), list1.end(),
+                next(list1.begin(), 2), list1.end())
+        << endl;
+}
+
