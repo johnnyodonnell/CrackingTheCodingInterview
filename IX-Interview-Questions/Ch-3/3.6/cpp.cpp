4d85608b8f819223a6f3461035f8888762ecc739
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-05-14 15:29:55 -0400

    Add solution for 3.6

---
+++ IX-Interview-Questions/Ch-3/3.6/cpp.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <list>
+
+
+using namespace std;
+
+struct Animal {
+    virtual void talk() = 0;
+};
+
+struct Dog : Animal {
+    void talk() {
+        cout << "Bark!" << endl;
+    }
+};
+
+struct Cat : Animal {
+    void talk() {
+        cout << "Meow!" << endl;
+    }
+};
+
+class Shelter {
+    private:
+        list<Animal*> q {};
+
+        template <typename T>
+        T dequeue() {
+            auto p = q.begin();
+            while (p != q.end()) {
+                T result = dynamic_cast<T>(*p);
+
+                if (result) {
+                    q.erase(p);
+                    return result;
+                }
+
+                p++;
+            }
+        }
+
+    public:
+        void enqueue(Animal* a) {
+            q.push_back(a);
+        }
+
+        Animal* dequeueAny() {
+            Animal* front = q.front();
+            q.pop_front();
+            return front;
+        }
+
+        Dog* dequeueDog() {
+            return dequeue<Dog*>();
+        }
+
+        Cat* dequeueCat() {
+            return dequeue<Cat*>();
+        }
+
+        bool empty() {
+            return q.empty();
+        }
+};
+
+int main() {
+    Shelter shelter {};
+    shelter.enqueue(new Dog());
+    shelter.enqueue(new Dog());
+    shelter.enqueue(new Cat());
+    shelter.enqueue(new Cat());
+    shelter.enqueue(new Cat());
+    shelter.enqueue(new Dog());
+    shelter.enqueue(new Cat());
+    shelter.enqueue(new Cat());
+    shelter.enqueue(new Dog());
+
+    shelter.dequeueCat()->talk();
+    shelter.dequeueDog()->talk();
+    cout << endl;
+
+    while (!shelter.empty()) {
+        Animal* a = shelter.dequeueAny();
+        a->talk();
+    }
+}
+
