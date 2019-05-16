6d184f567001c4aa4a93f777b61c3dd49e8f2732
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-05-16 10:36:35 -0400

    Add to implementation details script

--- IX-Interview-Questions/Ch-5/implementation-details.cpp
+++ IX-Interview-Questions/Ch-5/implementation-details.cpp
@@ -8,5 +8,19 @@ using namespace std;
 int main() {
     cout << "Bits in a byte: " << CHAR_BIT << endl;
     cout << "Bytes in an int: " << sizeof(int) << endl;
+
+    // https://stackoverflow.com/questions/16501091/detect-ones-or-twos-complement-architecture-in-c
+    if (-1 == ~0) {
+        cout << "Using two's complement" << endl;
+    } else {
+        // ~0 will become something like -0
+        cout << "Using one's complement" << endl;
+    }
+
+    if ((-4 >> 1) == (-4 / 2)) {
+        cout << "Using arithmetic shift" << endl;
+    } else {
+        cout << "Using logical shift" << endl;
+    }
 }
 
