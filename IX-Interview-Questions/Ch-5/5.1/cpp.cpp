efc3d1642794584a8101d53faea42441218bd652
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-05-16 10:48:56 -0400

    Add alt implementation to 5.1

--- IX-Interview-Questions/Ch-5/5.1/cpp.cpp
+++ IX-Interview-Questions/Ch-5/5.1/cpp.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <climits>
 
 
 using namespace std;
 
 int insert(const int n, const int m, const int i, const int j) {
+    /*
+     * Alt mask implementation
+     *
     int mask = -1;
     mask <<= j;
     mask = ~mask;
+    mask <<= i;
+    */
+    int mask = INT_MAX;
+    mask >>= (31 - j);
     mask <<= i;
     return (n & ~mask) | ((n << i) & mask);
 }
