4ea94e3a2e86d975ab98fb67d3d5ebb6ca47f057
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-05-18 12:45:47 -0400

    Add simple Deck OO design for 7.1

---
+++ IX-Interview-Questions/Ch-7/7.1/cpp.h
@@ -0,0 +1,41 @@
+#include <vector>
+
+
+enum class Suit { Spade, Club, Diamond, Heart, Joker };
+
+enum class Rank {
+    Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
+    Jack, Queen, King,
+};
+
+class Card {
+    private:
+        Suit suit;
+
+    public:
+        Suit get_suit();
+};
+
+class Standard : public Card {
+    private:
+        Rank rank;
+
+    public:
+        Rank get_rank();
+};
+
+class Joker : public Card {
+};
+
+class Deck {
+    private:
+        std::vector<Card> cards;
+
+    public:
+        Deck();
+
+        void shuffle();
+        const Card& draw();
+        void remove(const Card&);
+};
+
