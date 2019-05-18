f3559afd801638f992f3c08787f4ea4e4e3c38fa
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-05-18 13:25:38 -0400

    Add OO design for a Jigsaw puzzle

---
+++ IX-Interview-Questions/Ch-7/7.6/cpp.h
@@ -0,0 +1,14 @@
+
+class PuzzlePiece {
+};
+
+class Jigsaw {
+    private:
+        std::vector<std::vector<PuzzlePiece>> solution;
+        std::vector<PuzzlePiece> pieces;
+
+    public:
+        bool try_place(const PuzzlePiece&, int row, int col);
+        std::vector<PuzzlePiece> get_pieces();
+};
+
