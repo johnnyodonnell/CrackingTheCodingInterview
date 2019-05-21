9768190c04ccbec93a763c8ebf50bbbbd33336ac
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-05-20 20:35:10 -0400

    Add solution to 8.8
    Remove unnecessary header in 8.7

--- IX-Interview-Questions/Ch-8/8.8/cpp.cpp
+++ IX-Interview-Questions/Ch-8/8.8/cpp.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <algorithm>
 
 
 using namespace std;
 
-void print_permutations(const string& str, const string& new_str = {}) {
+void print_permutations(const string& str, const string& new_str) {
     if (str.empty()) {
         cout << new_str << endl;
     } else {
+        char prev_chr = 0;
         for (int i = 0; i < str.length(); i++) {
+            if (prev_chr && (prev_chr == str[i])) {
+                continue;
+            }
+
             auto str_copy = str;
             str_copy.erase(i, 1);
 
@@ -15,8 +21,16 @@ void print_permutations(const string& str, const string& new_str = {}) {
             new_str_copy.push_back(str[i]);
 
             print_permutations(str_copy, new_str_copy);
+
+            prev_chr = str[i];
         }
     }
+}
+
+void print_permutations(const string& str) {
+    auto str_copy = str;
+    sort(str_copy.begin(), str_copy.end());
+    print_permutations(str_copy, {});
 }
 
 int main() {
