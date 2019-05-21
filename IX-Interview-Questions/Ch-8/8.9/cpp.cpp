ccaf3908dd9f647003c346f280d99f1b06223693
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-05-20 20:55:08 -0400

    Add solution for 8.9

---
+++ IX-Interview-Questions/Ch-8/8.9/cpp.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <unordered_set>
+
+
+using namespace std;
+
+void print_parens(int n) {
+    if (n <= 0) {
+        cout << "Nothing to print" << endl;
+    } else {
+        unordered_set<string> combos {"()"};
+
+        for (int i = 1; i < n; i++) {
+            unordered_set<string> new_combos {};
+            for (auto& combo : combos) {
+                new_combos.insert("(" + combo + ")");
+                new_combos.insert("()" + combo);
+                new_combos.insert(combo + "()");
+            }
+            combos = new_combos;
+        }
+
+        for (auto& combo : combos) {
+            cout << combo << endl;
+        }
+    }
+}
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        print_parens(i);
+        cout << endl;
+    }
+};
