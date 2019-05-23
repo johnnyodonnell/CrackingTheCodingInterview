ca21c83aa6da9323654ad103f560d1957b7af50d
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-05-23 15:00:45 -0400

    Improved solution for 12.11

--- IX-Interview-Questions/Ch-12/12.11/c.c
+++ IX-Interview-Questions/Ch-12/12.11/c.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 void* my2DAlloc(size_t size_type, int r, int c) {
-    void** result = malloc(r * sizeof(void*));
+    void** rows = malloc(r * sizeof(void*));
+    void* cols = malloc(r * c * size_type);
 
     for (int i = 0; i < r; i++) {
-        result[i] = malloc(c * size_type);
+        rows[i] = cols + (i * c);
     }
 
-    return result;
+    return rows;
 }
 
 int main() {
-    char** arr = my2DAlloc(sizeof(int), 10, 10);
+    int rows = 3;
+    int cols = 4;
 
-    for (int r = 0; r < 10; ++r) {
-        for (int c = 0; c < 10; ++c) {
+    char** arr = my2DAlloc(sizeof(int), rows, cols);
+
+    for (int r = 0; r < rows; ++r) {
+        for (int c = 0; c < cols; ++c) {
             printf("(r: %d, c: %d)\n", r, c);
-            arr[r][c] = r + c;
+            arr[r][c] = (r * c) + c;
         }
     }
     printf("\n");
 
-    for (int r = 0; r < 10; ++r) {
-        for (int c = 0; c < 10; ++c) {
+    for (int r = 0; r < rows; ++r) {
+        for (int c = 0; c < cols; ++c) {
             printf("%d, ", arr[r][c]);
         }
         printf("\n");
