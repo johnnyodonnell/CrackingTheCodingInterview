272e61d1c2dbc53ae7c8a96f70fcca9a30e2cb95
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-05-23 15:08:59 -0400

    Improve solution 12.11

--- IX-Interview-Questions/Ch-12/12.11/c.c
+++ IX-Interview-Questions/Ch-12/12.11/c.c
@@ -3,21 +3,27 @@
 
 
 void* my2DAlloc(size_t size_type, int r, int c) {
-    void** rows = malloc(r * sizeof(void*));
-    void* cols = malloc(r * c * size_type);
+    size_t rows_size = r * sizeof(void*);
+    size_t cols_size = r * c * size_type;
+
+    void* mem = malloc(rows_size + cols_size);
+    void** rows = mem;
+    void* cols = (rows + r);
 
     for (int i = 0; i < r; i++) {
-        rows[i] = cols + (i * c);
+        // The reason this worked earlier without size_type is because
+        // we were using char instead of int
+        rows[i] = cols + (i * c * size_type);
     }
 
-    return rows;
+    return mem;
 }
 
 int main() {
-    int rows = 3;
-    int cols = 4;
+    int rows = 10;
+    int cols = 10;
 
-    char** arr = my2DAlloc(sizeof(int), rows, cols);
+    int** arr = my2DAlloc(sizeof(int), rows, cols);
 
     for (int r = 0; r < rows; ++r) {
         for (int c = 0; c < cols; ++c) {
