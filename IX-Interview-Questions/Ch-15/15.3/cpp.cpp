968fa251e63a260fdc2ea09db7b498547edda35e
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-05-23 19:10:34 -0400

    Add solution to 15.3

---
+++ IX-Interview-Questions/Ch-15/15.3/cpp.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <cstdlib>
+
+
+using namespace std;
+
+class Chopstick : public mutex {};
+
+void print(const string& str) {
+    static mutex m {};
+    m.lock();
+    cout << str << endl;
+    m.unlock();
+}
+
+class Philosopher {
+    private:
+        string id;
+        Chopstick& left_chopstick;
+        Chopstick& right_chopstick;
+
+    public:
+        Philosopher(
+                int c_id,
+                Chopstick& c_left_chopstick, Chopstick& c_right_chopstick)
+            :id{to_string(c_id)},
+            left_chopstick{c_left_chopstick},
+            right_chopstick{c_right_chopstick} {}
+
+        Philosopher(const Philosopher&) = delete;
+        Philosopher& operator=(const Philosopher&) = delete;
+
+        Philosopher(Philosopher&& p)
+            :id{p.id},
+            left_chopstick{p.left_chopstick},
+            right_chopstick{p.right_chopstick} {}
+
+        Philosopher& operator=(Philosopher&& p) = delete;
+
+        void eat_smart() {
+            lock(left_chopstick, right_chopstick);
+            print(string{"Philosopher "} + id + string{" left and right"});
+
+            print(string{"Philosopher "} + id);
+            left_chopstick.unlock();
+            right_chopstick.unlock();
+
+            eat_smart();
+        }
+
+        void eat() {
+            left_chopstick.lock();
+            print(string{"Philosopher "} + id + string{" left"});
+
+            right_chopstick.lock();
+            print(string{"Philosopher "} + id + string{" left and right"});
+
+            print(string{"Philosopher "} + id);
+            left_chopstick.unlock();
+            right_chopstick.unlock();
+
+            eat();
+        }
+};
+
+int main(int argc, char** argv) {
+    int num_of_philosophers = atoi(argv[1]);
+
+    vector<Philosopher> philosophers {};
+
+    auto first_chopstick = new Chopstick{};
+    auto current_chopstick = new Chopstick{};
+    Chopstick* previous_chopstick;
+
+    philosophers.push_back({1, *first_chopstick, *current_chopstick});
+
+    for (int i = 2; i < num_of_philosophers; i++) {
+        previous_chopstick = current_chopstick;
+        current_chopstick = new Chopstick{};
+
+        philosophers.push_back(
+                {i, *previous_chopstick, *current_chopstick});
+    }
+
+    philosophers.push_back(
+            {num_of_philosophers, *current_chopstick, *first_chopstick});
+
+    vector<thread> thrds {};
+    for (auto& philosopher : philosophers) {
+        thread thr {[&philosopher]() {
+            // philosopher.eat();
+            philosopher.eat_smart();
+        }};
+        thrds.push_back(move(thr));
+    }
+
+    for (auto& thr : thrds) {
+        thr.join();
+    }
+}
+
